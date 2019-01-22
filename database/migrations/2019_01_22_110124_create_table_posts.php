6c7c5bb82a29ac4c403e3b646de6b16b89a34aeb
mahmoudafifi414 <mahmoudafifi460@gmail.com>
2019-01-22 13:11:52 +0200

    commit the API with passport authentication

---
+++ database/migrations/2019_01_22_110124_create_table_posts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('post_body');
+            $table->longText('image')->nullable($value = true);
+            $table->text('user_id', 191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
